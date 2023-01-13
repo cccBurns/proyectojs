b73d80fbe3baea66273e4b944556436211852f1c
christian burns <christianburns03@gmail.com>
2023-01-13 09:19:02 -0600

    encontre el error de verCarrito y lo pude hacer andad

--- index.js
+++ index.js
@@ -33,47 +33,6 @@ productos.forEach((product) => {
     });
 });
 
-verCarrito.addEventListener("click", () => {
-    modalContainer.innerHTML = "";
-    modalContainer.style.display = "flex";
-    const modalHeader = document.createElement("div");
-    modalHeader.className = "modal-header";
-    modalHeader.innerHTML = `
-    <h1 class="modal-header-title">Carrito.</h1>
-    `;
-    modalContainer.append(modalHeader);
-
-    const modalbutton = document.createElement("h1");
-    modalbutton.innerText = "x";
-    modalbutton.className = "modal-header-button";
-
-    modalbutton.addEventListener("click", () => {
-        modalContainer.style.display = "none";
-    });
-
-    modalHeader.append(modalbutton);
-
-    carrito.forEach((product) => {
-        let carritoContent = document.createElement("div");
-        carritoContent.className = "modal-content";
-        carritoContent.innerHTML = `
-            <img src="${product.img}">
-            <h3>${product.nombre}</h3>
-            <p>${product.precio} $</p>
-            `;
-
-        modalContainer.append(carritoContent);
-    });
-
-    const total = carrito.reduce((acc, el) => acc + el.precio, 0);
-
-    const totalBuying = document.createElement("div");
-    totalBuying.className = "total-content";
-    totalBuying.innerHTML = `total a pagar: ${total} $`;
-    modalContainer.append(totalBuying);
-    
-});
-
 
 
 /* let  seleccion = prompt("Desea comprar si o no")

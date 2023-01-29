4dafdcd674593ebb36ab52f7f634bbea836bfda4
christian burns <christianburns03@gmail.com>
2023-01-28 18:12:29 -0600

    le agregue el sweet alert, fetch & ajax

--- carrito.js
+++ carrito.js
@@ -1,5 +1,5 @@
 // CARRITO
-    
+    console.log('carrito');
     const pintar_carrito = ()  => {
         modal_container.innerHTML = "";
         modal_container.style.display = "flex";
@@ -77,12 +77,23 @@
 
     const eliminar_producto = (id) => {
     const foundId = carrito.find((element) => element.id === id);
-
-        console.log(foundId);
-
-        carrito = carrito.filter((carritoId) => {
+    
+    
+    
+    carrito = carrito.filter((carritoId) => {
+        console.log('oundId.id' , foundId.id , 'carritoId' ,carritoId);
+        if (carritoId.id == foundId.id) {
+            Swal.fire({
+                icon: 'error',
+                title: '',
+                text: 'Error al eliminar el producto',                
+              })
+        }else{
+              Swal.fire('Producto Eliminado.')
+          }
         return carritoId !== foundId;
     });
+    
 
             carrito_counter();
             save_local();
@@ -102,3 +113,97 @@ const carritoLength = carrito.length;
   };
   
 carrito_counter();
+
+
+
+
+//OBTENER LISTA DE PRODUCTOS
+async function obtener_productos() {
+    let url = 'products.json';
+    try {
+        let respuesta = await fetch(url);        
+        return await respuesta.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+//OBJETENER PRODUCTOS
+
+async function render_productos() {
+    let lista_productos = await obtener_productos();
+    let html = '';
+    console.log(lista_productos);return;
+    lista_productos.forEach(user => {
+        console.log(user);return;
+        let htmlSegment = `<div class="user">
+                            <img src="${user.profileURL}" >
+                            <h2>${user.firstName} ${user.lastName}</h2>
+                            <div class="email"><a href="email:${user.email}">${user.email}</a></div>
+                        </div>`;
+
+        html += htmlSegment;
+    });
+
+    let container = document.querySelector('.container');
+    container.innerHTML = html;
+}
+
+//RENDER_PRODUCTOS
+
+//OBTENER VALOR DOLAR
+async function obtener_valor_dolar() {
+    let url = 'valor_dolar.json';
+    try {
+        let respuesta = await fetch(url);        
+        return await respuesta.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+async function render_valor_dolar() {
+    let datos_dolar = await obtener_valor_dolar();
+    console.log();
+    let html = '';
+    console.log(datos_dolar);
+    datos_dolar.forEach(dato => {
+        console.log(dato);
+        let htmlSegment = dato.valor;
+
+        html += htmlSegment;
+    });
+
+    let container = document.getElementById('valor_dolar');
+    console.log(container);
+    container.innerHTML = html;
+}
+
+render_valor_dolar();
+
+//OBTENER POKEMON
+async function obtener_pokemon() {
+    let url = 'valor_dolar.json';
+    let html = '';
+    try {
+        $.ajax({
+            url: "http://pokeapi.co/api/v2/pokemon/?limit=20",
+            method: 'GET',
+            cache: false,
+            success: function(data){              
+              let nombre_pokemon = data.results[3].name;
+              let container = document.getElementById('nombre_pokemon');
+              html += nombre_pokemon;
+              container.innerHTML = html;
+            },
+            error: function(data){
+                console.log(data);
+              },
+          });
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+obtener_pokemon();
